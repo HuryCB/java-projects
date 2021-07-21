37b35483f4c2b4a5e2ca787f287f51b28b5d30b4
HuryCB <huryelc.b@gmail.com>
2021-07-21 08:01:21 -0300

    Desafios de Nivel Intermediario

---
+++ dio-desafios/src/main/java/dio/intermediario/terceiro/desafio/DesafioIntermediarioTres.java
@@ -0,0 +1,30 @@
+package dio.intermediario.terceiro.desafio;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class DesafioIntermediarioTres {
+
+    public static void main(String[] args) throws IOException {
+            Scanner leitor = new Scanner(System.in);
+            double soma = 0;
+            char O = leitor.next().toUpperCase().charAt(0);
+            double[][] M = new double[12][12];
+            for (  int i =0; i < 12; i++             ) {
+                for (      int j = 0; j < 12; j++   ) {
+                    M[i][j] = leitor.nextDouble();
+                }
+            }
+
+            for ( int i =0; i < 12; i++ ) {
+                System.out.println("\n");
+                for ( int j = 0; j < 12; j++ ) {
+                    if   ( (j<i) && (j+i)<=10)  soma += M[i][j];
+                }
+            }
+
+            if (     O == 'M'                ) soma /= 30;
+            System.out.println(String.format("%.1f", soma));
+        }
+
+}
